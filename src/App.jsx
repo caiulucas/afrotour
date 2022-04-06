576b971aba409076a38374ad8dd663cc214a1a54
Caio Lucas <lienscarlet@gmail.com>
2022-04-06 17:32:18 -0300

    aula 05-04

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
+
 import { Navbar } from './components/Navbar';
 import { CityCard } from './components/CityCard';
 
 import './styles/global.scss';
-import './styles/navbar.scss';
 import './styles/app.scss';
-import './styles/cities.scss'
+import './styles/cities.scss';
 
 const cities = [
   {
+    id: 1,
     cityName: 'São Paulo',
     state: 'São Paulo',
     area: 1521,
@@ -17,6 +18,7 @@ const cities = [
     image: 'https://images.unsplash.com/photo-1629984557780-4dde2292e245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
   },
   {
+    id: 2,
     cityName: 'Rio de Janeiro',
     state: 'Rio de Janeiro',
     area: 1200,
@@ -24,6 +26,7 @@ const cities = [
     image: 'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
   },
   {
+    id: 3,
     cityName: 'Salvador',
     state: 'Bahia',
     area: 693,
@@ -31,6 +34,7 @@ const cities = [
     image: 'https://images.unsplash.com/photo-1533241818630-edad657eb3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
   },
   {
+    id: 4,
     cityName: 'Ouro Preto',
     state: 'Minas Gerais',
     area: 1.245,
@@ -48,14 +52,9 @@ export function App() {
         <h1>Brasil</h1>
         <div className="card-container">
           {cities.map(city => (
-            <CityCard 
-              cityName={city.cityName}
-              state={city.state}
-              area={city.area}
-              population={city.population}
-              image={city.image}
-            />
+            <CityCard key={city.id} city={city} />
           ))}
+  
         </div>
       </section>
     </div>
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './styles.scss'
 
-import { Button } from './Button';
+import { Button } from '../Button';
 
-import { ReactComponent as BrazilSvg } from '../assets/brazil.svg';
-import { ReactComponent as PortugalSvg } from '../assets/portugal.svg';
-import { ReactComponent as ItalySvg } from '../assets/italy.svg';
-import { ReactComponent as FranceSvg } from '../assets/france.svg';
+import { ReactComponent as BrazilSvg } from '../../assets/brazil.svg';
+import { ReactComponent as PortugalSvg } from '../../assets/portugal.svg';
+import { ReactComponent as ItalySvg } from '../../assets/italy.svg';
+import { ReactComponent as FranceSvg } from '../../assets/france.svg';
 
 export function Navbar() {
   return (
